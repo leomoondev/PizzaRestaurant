97bd7f4bdc7e6cbdd3972058393e8fad6f26ec4f
Leo Moon <leomoondev@gmail.com>
2017-02-12 16:25:16 -0800

    Completed the assignment

--- PizzaRestaurant/Kitchen.m
+++ PizzaRestaurant/Kitchen.m
@@ -10,9 +10,28 @@
 
 @implementation Kitchen
 
-/*- (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings
-{
-    
-}*/
+- (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings {
+
+    Pizza *pizza = [[Pizza alloc] initWithSize:size initWithToppings:toppings];
+    NSString *showPizza = [toppings componentsJoinedByString:@","];
+
+    switch(size) {
+        case small:
+            NSLog(@"This is small pizza with %@\n", showPizza);
+            break;
+        case medium:
+            NSLog(@"This is medium pizza with %@\n", showPizza);
+            break;
+        case large:
+            NSLog(@"This is large pizza with %@\n", showPizza);
+            break;
+        case extraLarge:
+            NSLog(@"This is extra large pizza with %@\n", showPizza);
+            break;
+        default:
+            NSLog(@"You've entered invalid size. Please try again!\n");
+    }
+    return pizza;
+}
 
 @end
